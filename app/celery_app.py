f7960b3483dd5d641179438b84152890587eeef0
Syaransh Chaurasia <sc23786@essex.ac.uk>
2025-08-11 12:10:57 +0100

    update

--- app/celery_app.py
+++ app/celery_app.py
@@ -1,12 +1,19 @@
 from celery import Celery
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
+
+# Get Redis URL from environment or fallback (update your env on Render)
+redis_url = os.getenv(
+    "REDIS_URL",
+    "redis://:<password>@redis-17850.c90.us-east-1-3.ec2.redns.redis-cloud.com:17850/0"
+)
 
 celery_app = Celery(
     "worker",
-    broker=os.getenv("CELERY_BROKER_URL"),
-    backend=os.getenv("CELERY_RESULT_BACKEND"),
+    broker=redis_url,
+    backend=redis_url
 )
 
 celery_app.conf.update(
